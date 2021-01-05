25305c55d75b23c73e10969a92ec40449bdbf9cf
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-01-05 23:33:41 +0300

    Update ConditionalStatement.java

--- startjava/ConditionalStatement.java
+++ startjava/ConditionalStatement.java
@@ -1,14 +1,11 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 21;
-        float growth = 2.0f;
-        boolean isMaleGender = true;
-        char firstLetterOfName = 'A';
-
         if (age > 20) {
-            System.out.println("Действуйте, Вам уже третий десяток!");
+            System.out.println("Продолжить авторизацию");
         }
 
+        boolean isMaleGender = true;
         if (isMaleGender) {
             System.out.println("Умный и сильный");
         }
@@ -16,21 +13,20 @@ public class ConditionalStatement {
         if (!isMaleGender) {
             System.out.println("Зато красивая");
         }
-
+        
+        float growth = 2.0f;
         if (growth < 1.80) {
             System.out.println("Идеальный рост для женщины");
-        }
-        else {
+        } else {
             System.out.println("Идеальный рост для мужчины");
         }
 
+        char firstLetterOfName = 'A';
         if (firstLetterOfName == 'M') {
             System.out.println("Тринадцатая буква в английском алфавите");
-        }
-        else if (firstLetterOfName == 'I') {
+        } else if (firstLetterOfName == 'I') {
             System.out.println("Девятая буква в английском алфавите");
-        }
-        else {
+        } else {
             System.out.println("Первая буква имени не M и не I");
         }
     }
