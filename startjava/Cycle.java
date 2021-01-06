67502896a7eee9fcce4fd670e67555983edfc816
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-01-06 23:28:41 +0300

    Update Cycle.java

--- startjava/Cycle.java
+++ startjava/Cycle.java
@@ -7,18 +7,18 @@ public class Cycle {
         int n = 6;
         while (n >= -6) {
             System.out.println(n);
-            n-=2;
+            n -= 2;
         }
 
         int z = 10;
-        int result = 0;
+        int sum = 0;
         do {
             z++;
             if (z % 2 != 0) {
-                result = result + z;
+                sum += z;
             }
         } while (z < 20);
-        System.out.println(result);
+        System.out.println(sum);
     }
 }
 
