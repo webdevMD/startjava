13fccfa41f3a98a9cfd58c0675f962687091cbed
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-01-13 14:00:03 +0300

    Update Calculator.java

--- startjava/Calculator.java
+++ startjava/Calculator.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Calculator {
     public static void main(String[] args) {
         int x = 8, y = 13;
-        int sum = x + y;
-        int difference = x - y;
-        int productOfnumbers = x * y;
-        int quotientOfDivision = x / y;
-        long exponentiation = (long) Math.pow(x, y);
-        int remainderOfTheDivision = x % y;
+        long exponentiation = 1;
+        for (long i = 1; i <= (long)y; i++) {
+        exponentiation = exponentiation * (long)x;
+        }
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter Operator (+, -, *, /, ^, %) : ");
-        char ch = scan.next().charAt(0);
+        char sign = scan.next().charAt(0);
         
-        if (ch == '+') {
-            System.out.println(sum);
-        } else if (ch == '-') {
-            System.out.println(difference);
-        } else if (ch == '*') {
-            System.out.println(productOfnumbers);
-        } else if (ch == '/') {
-            System.out.println(quotientOfDivision);
-        } else if (ch == '^') {
+        if (sign == '+') {
+            System.out.println(x + y);
+        } else if (sign == '-') {
+            System.out.println(x - y);
+        } else if (sign == '*') {
+            System.out.println(x * y);
+        } else if (sign == '/') {
+            System.out.println(x / y);
+        } else if (sign == '^') {
             System.out.println(exponentiation);
         } else {
-            System.out.println(remainderOfTheDivision);
+            System.out.println(x % y);
         }
     }
 }
