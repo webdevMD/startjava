233f95a61ecbab7cf1c6221fcf3dd8c6c399ebb0
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-01-30 21:41:39 +0300

    created object of type Wolf

---
+++ startjava/Lesson 2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+    boolean man = true;
+    String nickname = "Silver";
+    int weight = 23;
+    int age = 5;
+    String color = "gray";
+
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    String howl() {
+        return "Wooo-oo";
+    }
+
+    boolean hunt() {
+        System.out.println("well, hare, wait!");
+        return true;
+    }
+}
