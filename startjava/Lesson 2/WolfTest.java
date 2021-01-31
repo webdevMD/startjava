30eda87860e60b2336048e03e8c65ef04d078775
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-01-31 12:24:27 +0300

    assigned values ​​to fields

--- startjava/Lesson 2/WolfTest.java
+++ startjava/Lesson 2/WolfTest.java
@@ -1,10 +1,15 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
+        wolfOne.man = true;
         System.out.println("gender man = " + wolfOne.man);
+        wolfOne.nickname = "Silver";
         System.out.println("nickname = " + wolfOne.nickname);
+        wolfOne.weight = 23;
         System.out.println("weight = " + wolfOne.weight);
+        wolfOne.age = 5;
         System.out.println("age = " + wolfOne.age);
+        wolfOne.color = "gray";
         System.out.println("color = " + wolfOne.color);
         wolfOne.go();
         wolfOne.sit();
