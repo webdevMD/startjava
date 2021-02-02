38b363571acebd2f17283e87c57b921f44d064fc
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-02-02 13:24:40 +0300

    Update CalculatorTest.java

--- startjava/Lesson 2/CalculatorTest.java
+++ startjava/Lesson 2/CalculatorTest.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        boolean consider = true;
-
-        
-        while (consider) {
-            Calculator calculatorOne = new Calculator();
-            calculatorOne.setFirstNumber(2);
-            System.out.println("Введите первое число: " + calculatorOne.getFirstNumber());
-            System.out.print("Введите знак математической операции: ");
-            Scanner operation = new Scanner(System.in);
-            char sign = operation.next().charAt(0);
-            calculatorOne.setSecondNumber(10);
-            System.out.println("Введите второе число: " + calculatorOne.getSecondNumber());
-            calculatorOne.setSign(sign);
-            System.out.println("Хотите продолжить вычисления? [да/нет]: ");
+        boolean proceed = true;
+        String answer;
+        while (proceed) {
+            Calculator calculator = new Calculator();
+            System.out.print("Введите первое число: ");
             Scanner scanner = new Scanner(System.in);
-            String answer = scanner.nextLine();
-            calculatorOne.setAnswer(answer);
-                if (answer == "да") {
-                    consider = true;
-                } else {
-                    consider = false;
+            int firstNumber = scanner.nextInt();
+            calculator.setFirstNumber(firstNumber);
+            System.out.print("Введите знак математической операции: ");
+            char sign = scanner.next().charAt(0);
+            calculator.setSign(sign);
+            System.out.print("Введите второе число: ");
+            int secondNumber = scanner.nextInt();
+            calculator.setSecondNumber(secondNumber);
+            calculator.consider();
+            do {
+                System.out.println("Хотите продолжить вычисления? [да/нет]: ");
+                while (!scanner.hasNext()) {
+                scanner.next();
                 }
+                answer = scanner.nextline();
+            } while (answer == "да" || "нет");
         }
     }
 }
