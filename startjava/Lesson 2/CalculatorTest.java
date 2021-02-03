c56f5c60aaf618600af18658110561a2fb9c2015
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-02-03 11:01:40 +0300

    Update CalculatorTest.java

--- startjava/Lesson 2/CalculatorTest.java
+++ startjava/Lesson 2/CalculatorTest.java
@@ -2,28 +2,35 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        boolean proceed = true;
         String answer;
-        while (proceed) {
-            Calculator calculator = new Calculator();
-            System.out.print("Введите первое число: ");
-            Scanner scanner = new Scanner(System.in);
-            int firstNumber = scanner.nextInt();
-            calculator.setFirstNumber(firstNumber);
-            System.out.print("Введите знак математической операции: ");
-            char sign = scanner.next().charAt(0);
-            calculator.setSign(sign);
-            System.out.print("Введите второе число: ");
-            int secondNumber = scanner.nextInt();
-            calculator.setSecondNumber(secondNumber);
-            calculator.consider();
-            do {
-                System.out.println("Хотите продолжить вычисления? [да/нет]: ");
-                while (!scanner.hasNext()) {
-                scanner.next();
-                }
-                answer = scanner.nextline();
-            } while (answer == "да" || "нет");
+        String option1 = "yes";
+        String option2 = "no";
+        boolean right1 = false;
+        boolean right2 = false;
+
+        Calculator calculator = new Calculator();
+        System.out.print("vvedi");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        calculator.setFirstNumber(firstNumber);
+        System.out.print("vvedi ");
+        char sign = scanner.next().charAt(0);
+        calculator.setSign(sign);
+        System.out.print("vvedi ");
+        int secondNumber = scanner.nextInt();
+        calculator.setSecondNumber(secondNumber);
+        calculator.consider();
+        while (true) {
+            System.out.println("vvedi ");
+            answer = scanner.nextline();
+            if (answer == option1) {
+                right1 = false;
+            }
+            if (answer == option2) {
+                right1 = false;
+            } else {
+                System.out.println("vvedi ");
+            }
         }
     }
 }
