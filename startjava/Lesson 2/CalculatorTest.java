4c4f62a54759df1147b234091c7f463b4de855a2
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-02-05 12:46:57 +0300

    Update CalculatorTest.java

--- startjava/Lesson 2/CalculatorTest.java
+++ startjava/Lesson 2/CalculatorTest.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        boolean proceed = true;
-        String answer;
+        String[] option = new String[]{"no", "yes"};
+        boolean right = true;
+
         Calculator calculator = new Calculator();
-        System.out.print("Введите первое число: ");
-        Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
-        calculator.setFirstNumber(firstNumber);
-        System.out.print("Введите знак математической операции: ");
-        char sign = scanner.next().charAt(0);
-        calculator.setSign(sign);
-        System.out.print("Введите второе число: ");
-        int secondNumber = scanner.nextInt();
-        calculator.setSecondNumber(secondNumber);
-        calculator.consider();
-        do {
-            System.out.println("Хотите продолжить вычисления? [да/нет]: ");
-            answer = scanner.nextline();
-        } while (answer != "да"|| answer != "нет");
+        while (right) {
+            System.out.print("Введите первое число: ");
+            Scanner scanner = new Scanner(System.in);
+            int firstNumber = scanner.nextInt();
+            calculator.setFirstNumber(firstNumber);
+            System.out.print("Введите знак математической операции: ");
+            char sign = scanner.next().charAt(0);
+            calculator.setSign(sign);
+            System.out.print("Введите второе число: ");
+            int secondNumber = scanner.nextInt();
+            calculator.setSecondNumber(secondNumber);
+            calculator.consider();
+            String answer;
+            while (true) {
+                System.out.println("Хотите продолжить вычисления? [no/yes]: ");
+                answer = scanner.nextLine();
+                if (option[0].equals(answer)){
+                    right = false;
+                    break;
+                }
+                if (option[1].equals(answer)){
+                    right = true;
+                    break;
+                }
+            }
+        }
     }
 }
