1c40d5c6c16667d1ffaabbc905eaf0b2384e1077
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-02-26 15:33:59 +0300

    Update GuessNumberTest.java

--- startjava/Lesson 2/GuessNumberTest.java
+++ startjava/Lesson 2/GuessNumberTest.java
@@ -2,36 +2,39 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        String answerName1;
-        String answerName2;
-        GuessNumber game = new GuessNumber();
-        Player playerOne = new Player("answerName1");
-        Player playerTwo = new Player("answerName2");
-        System.out.println("what is your name playerOne? ");
-        Scanner scanner = new Scanner(System.in);
-        //scanner.nextLine();
-        answerName1 = scanner.nextLine();
-        System.out.println("ответил playerOne имя " + answerName1);
-        System.out.println("what is your name playerTwo? ");
-        answerName2 = scanner.nextLine();
-        System.out.println("ответил playerTwo имя " + answerName2);
+    String answerName1 = null;
+    String answerName2 = null;
+    System.out.println("what is your name playerOne? ");
+    Player playerOne = new Player(answerName1);
+    Scanner name = new Scanner(System.in);
+    answerName1 = name.nextLine();
+    playerOne.setName(answerName1);
+    System.out.println("name playerOne = " + playerOne.getName());
 
-        String[] option = new String[]{"no", "yes"};
-        String answerProceed = "no";
+    System.out.println("what is your name playerTwo? ");
+    Player playerTwo = new Player(answerName2);
+    answerName2 = name.nextLine();
+    playerTwo.setName(answerName2);
+    System.out.println("name playerTwo = " + playerTwo.getName());
+    GuessNumber game = new GuessNumber();
+    game.startGame();
 
-        public boolean proceed() {
-            do {
-                System.out.println("Хотите продолжить игру? [да/нет]:");
-                Scanner.nextLine();
-                answerProceed = scanner.nextLine();
-            } while (!option[0].equals(answerProceed) && !option[1].equals(answerProceed));
-            if (option[0].equals(answerProceed)){
-                answerProceed = "no";
-                break;
-            }
-            return true;
+    String[] option = new String[]{"no", "yes"};
+    String answerProceed = "no";
+    //public boolean proceed() {
+        do {
+            System.out.println("Хотите продолжить игру? [да/нет]:");
+            //Scanner.nextLine();
+            answerProceed = name.nextLine();
+        } while (!option[0].equals(answerProceed) && !option[1].equals(answerProceed));
+        if (option[0].equals(answerProceed)){
+            answerProceed = "no";
+            //break;
         }
-
-        game.startGame();
+        if (option[1].equals(answerProceed)) {
+            game.startGame();
+        }
+        //return true;
+        //}
     }
 }
