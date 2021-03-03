c5d930329eff1b2ab004ddaa348ef62216b17127
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-03-03 15:35:33 +0300

    Update GuessNumberTest.java

--- startjava/Lesson 2/GuessNumberTest.java
+++ startjava/Lesson 2/GuessNumberTest.java
@@ -2,23 +2,16 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        String answerName1 = null;
-        String answerName2 = null;
         System.out.println("what is your name playerOne? ");
-        
-        Scanner inName = new Scanner(System.in);
-        answerName1 = inName.nextLine();
-        Player playerOne = new Player(answerName1);
+        Scanner scan = new Scanner(System.in);
+        String name1 = scan.nextLine();
+        Player playerOne = new Player(name1);
         System.out.println("name playerOne = " + playerOne.getName());
-
         System.out.println("what is your name playerTwo? ");
-        
-        answerName2 = inName.nextLine();
-        Player playerTwo = new Player(answerName2);
+        String name2 = scan.nextLine();
+        Player playerTwo = new Player(name2);
         System.out.println("name playerTwo = " + playerTwo.getName());
         GuessNumber game = new GuessNumber(playerOne, playerTwo);
-        game.start();
-
         String[] option = new String[]{"no", "yes"};
         String answerProceed = "no";
 
@@ -26,12 +19,8 @@ public class GuessNumberTest {
             game.start();
             do {
                 System.out.println("Хотите продолжить игру? [да/нет]:");
-                answerProceed = inName.nextLine();
+                answerProceed = scan.nextLine();
             } while (!option[0].equals(answerProceed) && !option[1].equals(answerProceed));
-            if (option[0].equals(answerProceed)){
-                answerProceed = "no";
-                break;
-            }
-        } while (true);
+        } while (option[1].equals(answerProceed));
     }
 }
