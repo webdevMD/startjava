d9742d82b2883e030d6cb898ca611efb9263e946
webdevMD <40711540+webdevMD@users.noreply.github.com>
2021-03-04 11:58:53 +0300

    Update GuessNumber.java

--- startjava/Lesson 2/GuessNumber.java
+++ startjava/Lesson 2/GuessNumber.java
@@ -10,49 +10,42 @@ public class GuessNumber {
         this.playerTwo = playerTwo;
     }
 
+    Scanner scan = new Scanner(System.in);
+
     public void start() {
+        boolean numberOneRight = false;
+        boolean numberTwoRight = false;
+        int x = 1;
+        int y = 100;
+        int targetNumber = ThreadLocalRandom.current().nextInt(x,y);
+        System.out.println("комп загадал " + targetNumber);
         while (true) {
-            boolean numberOneRight = false;
-            boolean numberTwoRight = false;
-            int x = 1;
-            int y = 100;
-            int targetNumber = ThreadLocalRandom.current().nextInt(x,y);
-            System.out.println("комп загадал " + targetNumber);
-            System.out.println("playerOne введите число: ");
-            Scanner scan = new Scanner(System.in);
+            System.out.println(playerOne.getName() + " введите число: ");
             int number1 = scan.nextInt();
             playerOne.setNumber(number1);
-            System.out.println("number1player = " + playerOne.getNumber());
-        
             if (number1 < targetNumber) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
             } else if (number1 > targetNumber) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
-            }
-            if (number1 == targetNumber) {
-                numberOneRight = true;
-                System.out.println("Первый игрок угадал?" + numberOneRight);
+            } else {
+                if (number1 == targetNumber) {
+                    numberOneRight = true;
+                    System.out.println(playerOne.getName() + " угадал!");
+                } break;
             }
 
-            System.out.println("playerTwo введите число: ");
+            System.out.println(playerTwo.getName() + " введите число: ");
             int number2 = scan.nextInt();
             playerTwo.setNumber(number2);
-            System.out.println("number2player = " + playerTwo.getNumber());
-        
             if (number2 < targetNumber) {
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
             } else if (number2 > targetNumber) {
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
-            }
-            if (number2 == targetNumber) {
-                numberTwoRight = true;
-                System.out.println("Второй игрок угадал?" + numberTwoRight);
-            }
-            if (numberOneRight || numberTwoRight) {
-                System.out.println("Есть победитель! Конец игры");
-                break;
             } else {
-            System.out.println("Игроки должны попробовать еще раз");
+                if (number2 == targetNumber) {
+                    numberTwoRight = true;
+                    System.out.println(playerTwo.getName() + " угадал!");
+                } break;
             }
         }
     }
